d34be482728eb011013e48e0414a27851ecdf9df
danvosk <gorkem1928karagoz@gmail.com>
2026-03-04 03:40:55 +0300

    refactor: extract mapper interfaces and implementations

---
+++ src/main/java/com/gorkem/manuelmap/mapperImpl/CompanyMapperImpl.java
@@ -0,0 +1,51 @@
+package com.gorkem.manuelmap.mapperImpl;
+
+import com.gorkem.manuelmap.dto.request.CompanyRequestDto;
+import com.gorkem.manuelmap.dto.response.CompanyResponseDto;
+import com.gorkem.manuelmap.dto.response.UserResponseDto;
+import com.gorkem.manuelmap.entity.Company;
+import com.gorkem.manuelmap.mapper.CompanyMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class CompanyMapperImpl implements CompanyMapper {
+    @Override
+    public Company toEntity(CompanyRequestDto companyRequestDto) {
+        Company company = new Company();
+        company.setName(companyRequestDto.name());
+        company.setIndustry(companyRequestDto.industry());
+        company.setCity(companyRequestDto.city());
+        return company;
+    }
+
+    @Override
+    public CompanyResponseDto toResponseDto(Company company) {
+        List<UserResponseDto> userResponseDtos;
+
+        if (company.getUsers() != null) {
+            userResponseDtos = company.getUsers().stream()
+                    .map(user -> new UserResponseDto(
+                            user.getId(),
+                            user.getUsername(),
+                            user.getFirstName(),
+                            user.getLastName(),
+                            user.getEmail(),
+                            user.getAge(),
+                            user.getSalary(),
+                            company.getName()
+                    )).toList();
+        } else {
+            userResponseDtos = List.of();
+        }
+
+        return new CompanyResponseDto(
+                company.getId(),
+                company.getName(),
+                company.getIndustry(),
+                company.getCity(),
+                userResponseDtos
+        );
+    }
+}
